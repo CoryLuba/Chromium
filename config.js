9c9c637c5cab7493ed5a1e06d6e61c26bc90fc99
CoryLuba <32577626+CoryLuba@users.noreply.github.com>
2017-10-06 12:59:19 -0400

    Add initial files from V0.2

---
+++ config.js
@@ -0,0 +1,14 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Chromium",
+   "version": "0.2",
+   "homepage": "https://www.google.com/",
+   "enableNavBttns": true,
+   "enableHomeBttn": true,
+   "enableReloadBttn": true,
+   "enableLogoutBttn": true,
+   "termsOfService": "This version does NOT work with Netflix. Stay tuned!",
+   "rotation": "0",
+   "kioskEnabled": true
+};
